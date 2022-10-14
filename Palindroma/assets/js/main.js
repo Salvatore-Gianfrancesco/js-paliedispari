3215813be3c26442a4b8f66585c98f6e3ff85e70
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-14 12:20:05 +0200

    added javascript code - [palindrom]

---
+++ Palindroma/assets/js/main.js
@@ -0,0 +1,21 @@
+const word = prompt("Inserisci una parola");
+// console.log(word);
+
+const wordReversed = reverseWord(word);
+// console.log(wordReversed);
+
+if (word === wordReversed) {
+    console.log("La parola inserita è un palindromo");
+} else {
+    console.log("La parola inserita NON è un palindromo");
+}
+
+function reverseWord(word) {
+    word = word.split("");
+    // console.log(Array.isArray(word));
+    word = word.reverse();
+    word = word.join("");
+    // console.log(Array.isArray(word));
+
+    return word;
+}
