4a02c50a105be5b4968a9d04ddedb7ca844e2e6c
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-16 16:40:34 +0200

    added a graphic layout to the site

--- Palindroma/assets/js/main.js
+++ Palindroma/assets/js/main.js
@@ -1,5 +1,9 @@
+const labelEl = document.querySelector("input");
+const buttonEl = document.querySelector("button");
 const h1EL = document.querySelector("h1");
 
+// ONLY JAVASCRIPT
+/*
 const word = prompt("Inserisci una parola");
 // console.log(word);
 
@@ -16,6 +20,26 @@ if (wordTemp === wordReversed) {
     console.log("La parola inserita NON è un palindromo");
     h1EL.innerHTML = "La parola inserita NON è un palindromo";
 }
+*/
+
+buttonEl.addEventListener("click", function () {
+    const word = labelEl.value;
+    // console.log(word);
+
+    const wordTemp = word.toUpperCase();
+    // console.log(wordTemp);
+
+    const wordReversed = reverseWord(wordTemp);
+    // console.log(wordReversed);
+
+    if (wordTemp === wordReversed) {
+        // console.log("La parola inserita è un palindromo");
+        h1EL.innerHTML = "La parola inserita è un palindromo";
+    } else {
+        // console.log("La parola inserita NON è un palindromo");
+        h1EL.innerHTML = "La parola inserita NON è un palindromo";
+    }
+})
 
 function reverseWord(word) {
     word = word.split("");
--- Pari_Dispari/assets/js/main.js
+++ Pari_Dispari/assets/js/main.js
@@ -1,3 +1,5 @@
+// ONLY JAVASCRIPT
+/*
 const h1El = document.querySelector("h1");
 const h2El = document.querySelector("h2");
 
@@ -35,6 +37,37 @@ if (result === choice) {
     console.log("Hai perso!!");
     h1El.innerHTML = "Hai perso!!";
 }
+*/
+
+const containerEl = document.querySelector(".container");
+const buttonEl = document.querySelector("button");
+const checkChoiceEl = document.querySelector(".check_choice");
+const checkNumberEl = document.querySelector(".check_number");
+const h2El = document.querySelector("h2");
+
+buttonEl.addEventListener("click", function () {
+    const checkedChoice = document.querySelector('.check_choice:checked').value;
+    const checkedNumber = Number(document.querySelector('.check_number:checked').value);
+    // console.log(checkedChoice, checkedNumber);
+
+    const num = generateRandom();
+    // console.log(num, "in main");
+
+    const sum = num + checkedNumber;
+    // console.log(sum);
+
+    document.querySelector(".computer_number > div").innerHTML = num;
+    document.querySelector(".sum_number > div").innerHTML = sum;
+
+    const result = isEvenOdd(sum);
+    if (result === checkedChoice) {
+        console.log("Hai vinto!!");
+        h2El.innerHTML = "Hai vinto!!";
+    } else {
+        console.log("Hai perso!!");
+        h2El.innerHTML = "Hai perso!!";
+    }
+});
 
 function generateRandom() {
     const num = Math.floor(Math.random() * 5 + 1);
